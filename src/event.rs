cb9319a5051c7d54d6b3115bb8599c71a52bbd9c
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-01-11 07:08:38 -0800

    chore: add editorconfig

--- src/event.rs
+++ src/event.rs
@@ -6,28 +6,26 @@ const X_GITHUB_EVENT: &str = "X-GitHub-Event";
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum GitHubEvent {
-    Ping,
-    Push,
-    Create,
+	Ping,
+	Push,
+	Create,
 }
 
 impl<'r, 'a> FromRequest<'r, > for GitHubEvent {
-    type Error = ();
+	type Error = ();
 
-    fn from_request(request: &'r Request<'_>) -> Outcome<GitHubEvent, ()> {
-        let keys = request.headers().get(X_GITHUB_EVENT).collect::<Vec<_>>();
-        if keys.len() != 1 {
-            return Outcome::Failure((Status::BadRequest, ()));
-        }
-       
-        let event = match keys[0] {
-            "ping" => GitHubEvent::Ping,
-            "push" => GitHubEvent::Push,
-            "create" => GitHubEvent::Create,
-            _ => {
-                return Outcome::Failure((Status::Ok, ()));
-            }
-        };
-        Outcome::Success(event)
-    }
+	fn from_request(request: &'r Request<'_>) -> Outcome<GitHubEvent, ()> {
+		let keys = request.headers().get(X_GITHUB_EVENT).collect::<Vec<_>>();
+		if keys.len() != 1 {
+			return Outcome::Failure((Status::BadRequest, ()));
+		}
+
+		let event = match keys[0] {
+			"ping" => GitHubEvent::Ping,
+			"push" => GitHubEvent::Push,
+			"create" => GitHubEvent::Create,
+			_ => { return Outcome::Failure((Status::Ok, ())); }
+		};
+		Outcome::Success(event)
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,18 @@ mod verification;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![
-        root,
-        webhook,
-    ])
+	rocket::build().mount("/", routes![
+		root,
+		webhook,
+	])
 }
 
 #[get("/")]
 fn root() -> &'static str {
-    "read if cute"
+	"read if cute"
 }
 
 #[post("/github", data = "<payload>")]
 fn webhook(event: GitHubEvent, payload: i32) -> &'static str {
-    "Hello, world!"
+	"Hello, world!"
 }
